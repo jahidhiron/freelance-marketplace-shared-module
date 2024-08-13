a4ddcb089e690bed196f568d1a73c12e5d00334d
Jahid Hiron <jahid.hiron@teamlance.io>
2024-08-13 19:29:31 +0600

    issue: cloudinary-upload.ts file issue

--- src/cloudinary-upload.ts
+++ src/cloudinary-upload.ts
@@ -1,24 +1,23 @@
 import cloudinary, {
-  UploadApiResponse,
   UploadApiErrorResponse,
+  UploadApiResponse,
 } from 'cloudinary';
 
-interface IFileOptions {
-  public_id?: string;
-  overwrite?: boolean;
-  invalidate?: boolean;
-  chunk_size?: number;
-  resource_type: 'image' | 'video' | 'raw' | 'auto';
-}
-
-const upload = (
+export function uploads(
   file: string,
-  options: IFileOptions
-): Promise<UploadApiResponse | UploadApiErrorResponse | undefined> => {
+  public_id?: string,
+  overwrite?: boolean,
+  invalidate?: boolean
+): Promise<UploadApiResponse | UploadApiErrorResponse | undefined> {
   return new Promise((resolve) => {
     cloudinary.v2.uploader.upload(
       file,
-      { ...options },
+      {
+        public_id,
+        overwrite,
+        invalidate,
+        resource_type: 'auto', // zip, images
+      },
       (
         error: UploadApiErrorResponse | undefined,
         result: UploadApiResponse | undefined
@@ -28,20 +27,6 @@ const upload = (
       }
     );
   });
-};
-
-export function uploads(
-  file: string,
-  public_id?: string,
-  overwrite?: boolean,
-  invalidate?: boolean
-): Promise<UploadApiResponse | UploadApiErrorResponse | undefined> {
-  return upload(file, {
-    public_id,
-    overwrite,
-    invalidate,
-    resource_type: 'auto',
-  });
 }
 
 export function videoUpload(
@@ -50,11 +35,23 @@ export function videoUpload(
   overwrite?: boolean,
   invalidate?: boolean
 ): Promise<UploadApiResponse | UploadApiErrorResponse | undefined> {
-  return upload(file, {
-    public_id,
-    overwrite,
-    invalidate,
-    chunk_size: 50000, // large files are devided into 50MB chunk
-    resource_type: 'video',
+  return new Promise((resolve) => {
+    cloudinary.v2.uploader.upload(
+      file,
+      {
+        public_id,
+        overwrite,
+        invalidate,
+        chunk_size: 50000,
+        resource_type: 'video',
+      },
+      (
+        error: UploadApiErrorResponse | undefined,
+        result: UploadApiResponse | undefined
+      ) => {
+        if (error) resolve(error);
+        resolve(result);
+      }
+    );
   });
 }
